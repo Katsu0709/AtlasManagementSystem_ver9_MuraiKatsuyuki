971d522daf797ed0c4f9d24c291f1b70a4458e98
katsu-0709-admin <tea0cl@gmail.com>
2026-04-07 00:04:50 +0900

    コメント数の表示とコメント入力のバリデーション設定完了しました！

---
+++ app/Http/Requests/CommentFormRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'comment' => 'required|max:250|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'comment.required' => 'コメントは必ず入力してください。',
+            'comment.max' => 'コメントは文字列で入力してください。',
+            'comment.string' => 'コメントは250文字以内で入力してください。',
+        ];
+    }
+}
--- app/Models/Posts/Post.php
+++ app/Models/Posts/Post.php
@@ -43,6 +43,7 @@ class Post extends Model
         return Post::with('postComments')->find($post_id)->postComments();
     }
 
+    // いいね数
     public function likes()
     {
         return $this->hasMany('App\Models\Posts\Like', 'like_post_id');
